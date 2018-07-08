33c1af9afe4c19cf7d61533cb97ce66623d2a3b2
Ranjan Shrestha <ranjan@sharingapples.com>
2018-07-08 10:43:50 +0545

    Add scripts to convert between ad and bs from command line

---
+++ scripts/ad2bs.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const NepaliDate = require('../cjs/NepaliDate').default;
+
+const ad = process.argv[2] ? new Date(process.argv[2]) : new Date();
+if (Number.isNaN(ad.getTime())) {
+  console.log(`Invalid date input '${process.argv[2]}'`);
+  process.exit(1);
+}
+if (ad < NepaliDate.minimum() || ad > NepaliDate.maximum()) {
+  console.log(`Date must be within ${NepaliDate.minimum().toDateString()} to ${NepaliDate.maximum().toDateString()}`);
+  process.exit(1);
+}
+
+const n = new NepaliDate(ad);
+console.log(n.toString());
---
+++ scripts/bs2ad.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const NepaliDate = require('../cjs/NepaliDate').default;
+
+function pad(n) {
+  if (n < 10) {
+    return `0${n}`;
+  }
+  return `${n}`;
+}
+
+function formatDate(d) {
+  return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}`;
+}
+
+try {
+  const bs = process.argv[2] ? new NepaliDate(process.argv[2]) : new NepaliDate();
+  console.log(formatDate(bs.getEnglishDate()));
+} catch (err) {
+  console.log(err.message);
+  process.exit(1);
+}
