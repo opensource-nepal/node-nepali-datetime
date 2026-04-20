ab1ba7e4700e157ff8815738f2c6bcaffdc46c85
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-04-20 21:34:13 +0545

    style: format oxlint.config.ts and package.json for consistency

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "oxlint";
+import { defineConfig } from 'oxlint'
 
 export default defineConfig({
-  plugins: ["import", "typescript", "unicorn"],
+    plugins: ['import', 'typescript', 'unicorn'],
 
-  ignorePatterns: ["**/*.{mjs,cjs,js,d.ts,d.mts}", ".next"],
-  overrides: [
-    {
-      files: ["*.test.ts", "*.spec.ts"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-      },
+    ignorePatterns: ['**/*.{mjs,cjs,js,d.ts,d.mts}', '.next'],
+    overrides: [
+        {
+            files: ['*.test.ts', '*.spec.ts'],
+            rules: {
+                '@typescript-eslint/no-explicit-any': 'off',
+            },
+        },
+    ],
+    rules: {
+        eqeqeq: 'warn',
+        'no-thenable': 'off',
     },
-  ],
-  rules: {
-    eqeqeq: "warn",
-    "no-thenable": "off",
-  },
-});
+})
